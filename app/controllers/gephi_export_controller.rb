df610c4ad77ef1c983aa05f4db50d0b73b1f1b2c
Devin Gaffney <itsme@devingaffney.com>
2012-11-19 10:35:40 -0800

    woo

--- app/controllers/gephi_export_controller.rb
+++ app/controllers/gephi_export_controller.rb
@@ -1,13 +1,17 @@
 class GephiExportController < ApplicationController
-  
+  skip_before_filter :verify_authenticity_token
+
   def index
     t = Time.now
-    filename = "tmp/#{Time.now.to_i+rand(1000000)}.gexf"
-    file = File.new(filename, "w")
-    file.write(params[:data].read)
+    filename = "#{File.dirname(__FILE__)}/../../tmp/#{Time.now.to_i+rand(1000000)}.gexf"
+    logger.info(filename)
+    data = params[:data].read
+    file = File.open(filename, "w")
+    file.write(data)
     file.close
-    `java -jar public/gephi_layout_script/ToolkitDemos.jar #{filename} #{filename.gsub(".gexf", "_new.gexf")}`
-    `rm #{filename}`
+    logger.info("java -jar -Xmx256M public/gephi_layout_script/ToolkitDemos.jar #{filename} #{filename.gsub(".gexf", "_new.gexf")}")
+    logger.info(`java -jar -Xmx256M public/gephi_layout_script/ToolkitDemos.jar #{filename} #{filename.gsub(".gexf", "_new.gexf")}`)
+    #`rm #{filename}`
     tt = Time.now
     logger.info("Gephi Layout Execution Time: #{tt-t}")
     render :xml => File.read(filename.gsub(".gexf", "_new.gexf"))
@@ -17,6 +21,12 @@ end
 # RestClient::Request.execute(:method => :post, :url => "http://127.0.0.1:3000/gephi_export", :timeout => 3600*24, :open_timeout => 10)
 
 # resource = RestClient::Resource.new(
-#   "http://murmuring-woodland-5601.herokuapp.com/gephi_export",
+#   "http://devingaffney.com/gephi_export",
 #   :timeout => -1)
 # response = resource.post :data =>  File.new("curation_1091_2906.gexf")
+
+# resource = RestClient::Resource.new(
+#   "http://127.0.0.1:3000/gephi_export",
+#   :timeout => -1)
+# response = resource.post :data =>  File.new("curation_1091_2906.gexf")
+
