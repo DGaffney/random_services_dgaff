66447ab42dd59933fbbc8ac5cdbb1fce10290c3e
Devin Gaffney <itsme@devingaffney.com>
2012-11-19 19:24:07 -0800

    woo

--- app/controllers/gephi_export_controller.rb
+++ app/controllers/gephi_export_controller.rb
@@ -21,9 +21,9 @@ end
 # RestClient::Request.execute(:method => :post, :url => "http://127.0.0.1:3000/gephi_export", :timeout => 3600*24, :open_timeout => 10)
 
 # resource = RestClient::Resource.new(
-#   "http://devingaffney.com/gephi_export",
+#   "http://178.79.169.159:23672/gephi_export",
 #   :timeout => -1)
-# response = resource.post :data =>  File.new("curation_1091_2906.gexf")
+# response = resource.post :data =>  File.new("curation_1133_3023.gexf")
 
 # resource = RestClient::Resource.new(
 #   "http://127.0.0.1:3000/gephi_export",
